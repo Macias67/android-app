779335a0fbed81cfb1473390aaa0a49517c28131
Luis Macias <luismacias.angulo@gmail.com>
2015-08-19 00:19:28 -0500

    Diseño de formulario de producto casi listo

--- public/assets/admin/pages/app/cliente/productos/nuevo-producto.js
+++ public/assets/admin/pages/app/cliente/productos/nuevo-producto.js
@@ -4,9 +4,111 @@
 
 var NuevoProducto = function () {
 
-    var inputMask = function () {
-        $("input[name='codigo_postal']").inputmask("mask", {
-            "mask": "99999"
+    var inicio;
+    var fin;
+
+    var maxLenght = function () {
+        $("textarea[name='descripcion']").maxlength({
+            limitReachedClass: "label label-danger",
+            alwaysShow:        true
+        });
+
+        $("textarea[name='descripcion_corta']").maxlength({
+            limitReachedClass: "label label-danger",
+            alwaysShow:        true
+        });
+    }
+
+    var touchSpin = function () {
+        $("#precio").TouchSpin({
+            buttondown_class: 'btn blue',
+            buttonup_class:   'btn blue',
+            min:              0,
+            max:              1000000000,
+            step:             0.1,
+            decimals:         2,
+            boostat:          5,
+            maxboostedstep:   10,
+            prefix:           '$'
+        });
+
+        $("#cantidad").TouchSpin({
+            buttondown_class: 'btn blue',
+            buttonup_class:   'btn blue',
+            min:              0,
+            max:              1000000000,
+            step:             1,
+            boostat:          5,
+            maxboostedstep:   10
+        });
+    }
+
+    var dateRange = function () {
+        moment.locale('es');
+        var formato = 'DD/MMMM/YY';
+        $('#reportrange').daterangepicker({
+                opens:              'left',
+                startDate:           moment(),
+                endDate:             moment().add(29, 'days'),
+                showDropdowns:       true,
+                showWeekNumbers:     true,
+                timePicker:          true,
+                timePickerIncrement: 1,
+                timePicker12Hour:    true,
+                ranges:              {
+                    'Hoy':        [moment(), moment()],
+                    'Mañana':    [moment(), moment().add(1, 'days')],
+                    '7 Días':  [moment(), moment().add(6, 'days')],
+                    'Un Mes': [moment(), moment().add(29, 'days')],
+                    '6 Meses':   [moment(), moment().add(6, 'month')],
+                    '1 Año':   [
+                        moment(),
+                        moment().add(1, 'year')
+                    ]
+                },
+                buttonClasses:       ['btn'],
+                applyClass:          'green',
+                cancelClass:         'default',
+                format:              'DD/MM/YYYY',
+                separator:           ' al ',
+                locale:              {
+                    applyLabel:       'Aplicar',
+                    fromLabel:        'Desde',
+                    toLabel:          'a',
+                    customRangeLabel: 'Otro Rango',
+                    daysOfWeek:       ['D', 'L', 'M', 'I', 'J', 'V', 'S'],
+                    monthNames:       [
+                        'Enero',
+                        'Febrero',
+                        'Marzo',
+                        'Abril',
+                        'Mayo',
+                        'Junio',
+                        'Julio',
+                        'Agosto',
+                        'Septiembre',
+                        'Octubre',
+                        'Noviembre',
+                        'Diciembre'
+                    ],
+                    firstDay:         1
+                }
+            },
+            function (start, end) {
+                $('#reportrange span').html(start.format(formato) + ' al ' + end.format(formato));
+                inicio = start.format("MM-DD-YYYY HH:mm:ss");
+                fin = end.format("MM-DD-YYYY HH:mm:ss");
+            }
+        );
+        //Set the initial state of the picker label
+        $('#reportrange span').html(moment().format(formato) + ' al ' + moment().add(29, 'days').format(formato));
+        inicio = moment().format("MM-DD-YYYY HH:mm:ss");
+        fin = moment().add(29, 'days').format("MM-DD-YYYY HH:mm:ss");
+    }
+
+    var submitForm = function() {
+        $('#agregar').on('click', function() {
+            alert(inicio);
         });
     }
 
@@ -20,35 +122,35 @@ var NuevoProducto = function () {
             ignore:       "",  // validate all fields including form hidden input
             rules:        {
                 propietario_id: {
-                    required:  true
+                    required: true
                 },
-                nombre:   {
+                nombre:         {
                     required:  true,
                     maxlength: 45
                 },
-                calle:    {
+                calle:          {
                     required:  true,
                     maxlength: 14
                 },
-                numero:    {
+                numero:         {
                     required:  true,
                     maxlength: 5
                 },
-                colonia: {
-                    required: true,
+                colonia:        {
+                    required:  true,
                     maxlength: 45
                 },
-                codigo_postal: {
-                    required: true,
+                codigo_postal:  {
+                    required:  true,
                     maxlength: 45
                 },
-                referencia: {
+                referencia:     {
                     maxlength: 45
                 },
-                ciudad_id: {
-                    required:  true
+                ciudad_id:      {
+                    required: true
                 },
-                latlng_gmaps: {
+                latlng_gmaps:   {
                     required:  true,
                     maxlength: 45
                 },
@@ -90,11 +192,11 @@ var NuevoProducto = function () {
                             type:               "success",
                             animation:          'slide-from-top',
                             showCancelButton:   true,
-                            cancelButtonText: "Añadir nuevo cliente",
+                            cancelButtonText:   "Añadir nuevo cliente",
                             confirmButtonColor: Metronic.getBrandColor('green'),
                             confirmButtonText:  "Listado de clientes"
                         }, function (isConfirm) {
-                            if(isConfirm){
+                            if (isConfirm) {
                                 window.location.href = data.url;
                             } else {
                                 location.reload(true);
@@ -119,7 +221,10 @@ var NuevoProducto = function () {
 
     return {
         init: function () {
-            inputMask();
+            maxLenght();
+            touchSpin();
+            dateRange();
+            submitForm();
             handleForm();
         }
     }
--- resources/views/cliente/productos/form-nuevo.blade.php
+++ resources/views/cliente/productos/form-nuevo.blade.php
@@ -5,6 +5,7 @@
 	<link href="{{asset('assets/global/plugins/bootstrap-select/bootstrap-select.min.css')}}" rel="stylesheet" type="text/css"/>
 	<link href="{{asset('assets/global/plugins/select2/select2.css')}}" rel="stylesheet" type="text/css"/>
 	<link href="{{asset('assets/global/plugins/jquery-multi-select/css/multi-select.css')}}" rel="stylesheet" type="text/css"/>
+	<link href="{{asset('assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css')}}" rel="stylesheet" type="text/css"/>
 @stop
 
 {{-- Sobreescribir el sidebar
@@ -15,9 +16,9 @@
 
 {{-- Sobreescribir el título de pagina--}}
 @section('page-title')
-	  <h1>Registro de nuevo producto
-			<small>Información básica del producto</small>
-	  </h1>
+	<h1>Registro de nuevo producto
+		<small>Información básica del producto</small>
+	</h1>
 @stop
 
 {{-- Sobreescribir el toolbar de pagina
@@ -25,148 +26,151 @@
 
 {{-- Sobreescribir el breadcrumb de pagina --}}
 @section('page-breadcrumb')
-	  <ul class="page-breadcrumb breadcrumb">
-			<li>
-				  <a href="">Inicio</a>
-				  <i class="fa fa-circle"></i>
-			</li>
-			<li>
-				  <a href="{{route('productos-cliente')}}">Productos</a>
-				  <i class="fa fa-circle"></i>
-			</li>
-			<li>
-				  <a href="">Nuevo</a>
-			</li>
-	  </ul>
+	<ul class="page-breadcrumb breadcrumb">
+		<li>
+			<a href="">Inicio</a>
+			<i class="fa fa-circle"></i>
+		</li>
+		<li>
+			<a href="{{route('productos-cliente')}}">Productos</a>
+			<i class="fa fa-circle"></i>
+		</li>
+		<li>
+			<a href="">Nuevo</a>
+		</li>
+	</ul>
 @stop
 
 {{-- Conteindo de la vista. --}}
 @section('content')
-	  <div class="row">
-			<div class="col-md-12">
-				  <!-- BEGIN Portlet PORTLET-->
-				  <div class="portlet light animated bounceInUp">
-						<div class="portlet-title">
-							  <div class="caption">
-									<i class="icon-bag"></i>
-									<span class="caption-subject bold uppercase"> Prducto</span>
-									<span class="caption-helper">Registro de producto</span>
-							  </div>
-							  <div class="actions">
-									<a href="javascript:;" class="btn btn-circle btn-default btn-icon-only fullscreen"></a>
-							  </div>
+	<div class="row">
+		<div class="col-md-12">
+			<!-- BEGIN Portlet PORTLET-->
+			<div class="portlet light animated bounceInUp">
+				<div class="portlet-title">
+					<div class="caption">
+						<i class="icon-bag"></i>
+						<span class="caption-subject bold uppercase"> Prducto</span>
+						<span class="caption-helper">Registro de producto</span>
+					</div>
+					<div class="actions">
+						<button class="btn btn-circle bg-green-turquoise" id="agregar"><i class="fa fa-plus"></i> Agregar </button>
+						<a href="javascript:;" class="btn btn-circle btn-default btn-icon-only fullscreen" data-original-title="" title=""></a>
+					</div>
+				</div>
+				<div class="portlet-body form">
+					{!! Form::open($param) !!}
+					<div class="col-md-6">
+						<div class="form-body">
+							<div class="form-group">
+								<label class="col-md-3 control-label">Negocio</label>
+
+								<div class="col-md-9">
+									{!! Form::select('cliente_id', $negocios, NULL, ['class' => 'form-control']) !!}
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Producto: </label>
+
+								<div class="col-md-9">
+									<div class="input-icon">
+										<i class="fa fa-star"></i>
+										<input type="text" class="form-control" name="nombre" placeholder="Nombre del producto">
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-3 control-label">Slug</label>
+
+								<div class="col-md-9">
+									<div class="input-icon">
+										<i class="fa fa-desktop"></i>
+										<input type="text" class="form-control" name="slug" placeholder="Url del producto">
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3">Descripción</label>
+
+								<div class="col-md-9">
+									<textarea class="form-control" name="descripcion" maxlength="255" rows="3" style="resize: none;"></textarea>
+									<span class="help-block">Descripción detallada del producto. </span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3">Descripción corta</label>
+
+								<div class="col-md-9">
+									<textarea class="form-control" name="descripcion_corta" maxlength="45" rows="2" style="resize: none;"></textarea>
+									<span class="help-block">Descripción resumida del producto. </span>
+								</div>
+							</div>
 						</div>
-						<div class="portlet-body form">
-							{!! Form::open($param) !!}
-                            <div class="col-md-6">
-                                <div class="form-body">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">Negocio</label>
-                                        <div class="col-md-9">
-                                            {!! Form::select('cliente_id', $negocios, NULL, ['class' => 'form-control']) !!}
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">Producto: </label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon">
-                                                <i class="fa fa-star"></i>
-                                                <input type="text" class="form-control" name="nombre" placeholder="Nombre del producto">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">Slug</label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon">
-                                                <i class="fa fa-desktop"></i>
-                                                <input type="text" class="form-control" name="slug" placeholder="Url del producto">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Descripción</label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" name="descripcion" rows="3" style="resize: none;"></textarea>
-                                            <span class="help-block">Descripción detallada del producto. </span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Descripción corta</label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" name="descripcion_corta" rows="2" style="resize: none;"></textarea>
-                                            <span class="help-block">Descripción resumida del producto. </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-body">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Estatus</label>
-                                        <div class="col-md-9">
-                                            <input type="checkbox" class="make-switch" name="estatus"
-                                                   data-size="small"
-                                                   data-on-text="Online" data-off-text="Offline"
-                                                   data-on-color="success"
-                                                   data-off-color="default">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Precio</label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon input-small">
-                                                <i class="fa fa-usd"></i>
-                                                <input type="text" class="form-control" name="precio" placeholder="Precio">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Cantidad</label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon input-small">
-                                                <i class="fa fa-circle-o"></i>
-                                                <input type="text" class="form-control" name="cantidad" placeholder="Cantidad">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <h4 class="form-section">Disposición del Producto</h4>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Inicio</label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon">
-                                                <i class="fa fa-calendar"></i>
-                                                <input type="text" class="form-control" name="disp_inicio" placeholder="Fecha de inicio">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Termino</label>
-                                        <div class="col-md-9">
-                                            <div class="input-icon">
-                                                <i class="fa fa-calendar"></i>
-                                                <input type="text" class="form-control" name="disp_fin" placeholder="Fecha de termino">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-actions">
-                                    <div class="row">
-                                        <div class="col-md-offset-2 col-md-10">
-                                            <button type="submit" class="btn green">Registrar</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            </form>
-                            <div class="clearfix"></div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-body">
+							<div class="form-group">
+								<label class="control-label col-md-3">Estatus</label>
+
+								<div class="col-md-9">
+									<input type="checkbox" class="make-switch" name="estatus"
+									       data-size="small"
+									       data-on-text="Online" data-off-text="Offline"
+									       data-on-color="success"
+									       data-off-color="default">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3">Precio</label>
+
+								<div class="col-md-9">
+									<div class="input-inline input-medium">
+										<input id="precio" type="text" name="precio" class="form-control">
+									</div>
+									<span class="help-block">Solo dos deciamles (99.99) </span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3">Cantidad</label>
+
+								<div class="col-md-9">
+									<div class="input-inline input-medium">
+										<input id="cantidad" type="text" name="cantidad" class="form-control">
+									</div>
+									<span class="help-block">Unidades enteras</span>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label class="control-label col-md-3">Disponibilidad</label>
+
+								<div class="col-md-9">
+									<div id="reportrange" class="btn default">
+										<i class="fa fa-calendar"></i>
+										&nbsp;
+										<span></span>
+										<b class="fa fa-angle-down"></b>
+									</div>
+								</div>
+							</div>
 						</div>
-				  </div>
-				  <!-- END Portlet PORTLET-->
+					</div>
+					<div class="col-md-12">
+						<div class="form-actions">
+							<div class="row">
+								<div class="col-md-offset-2 col-md-10">
+									<button type="submit" class="btn green">Registrar</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					</form>
+					<div class="clearfix"></div>
+				</div>
 			</div>
-	  </div>
+			<!-- END Portlet PORTLET-->
+		</div>
+	</div>
 @stop
 
 {{-- Sobreescribir el encabezado de pagina
@@ -174,22 +178,23 @@
 
 {{-- Cargar los plugins de js --}}
 @section('plugins-core-js')
-{!! \Html::script('assets/global/plugins/jquery-inputmask/dist/inputmask/jquery.inputmask.min.js', array('type' => 'text/javascript')) !!}
-{!! \Html::script('assets/global/plugins/jquery-validation/js/jquery.validate.min.js', array('type' => 'text/javascript')) !!}
-{!! \Html::script('assets/global/plugins/jquery-validation/js/localization/messages_es.js', array('type' => 'text/javascript')) !!}
-{{--<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>--}}
-{{--{!! \Html::script('assets/global/plugins/gmaps/gmaps.min.js', array('type' => 'text/javascript')) !!}--}}
+	<script src="{{asset('assets/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js')}}" type="text/javascript"></script>
+	<script src="{{asset('assets/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js')}}" type="text/javascript"></script>
+	<script src="{{asset('assets/global/plugins/moment-with-locales.min.js')}}" type="text/javascript"></script>
+	<script src="{{asset('assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js')}}" type="text/javascript"></script>
+	{!! \Html::script('assets/global/plugins/jquery-validation/js/jquery.validate.min.js', array('type' => 'text/javascript')) !!}
+	{!! \Html::script('assets/global/plugins/jquery-validation/js/localization/messages_es.js', array('type' => 'text/javascript')) !!}
 @stop
 
 {{-- Cargar los archivos de js  --}}
 @section('page-level-js')
-{!! \Html::script('assets/global/plugins/bootstrap-select/bootstrap-select.min.js', array('type' => 'text/javascript')) !!}
-{!! \Html::script('assets/global/plugins/select2/select2.min.js', array('type' => 'text/javascript')) !!}
-{!! \Html::script('assets/global/plugins/jquery-multi-select/js/jquery.multi-select.js', array('type' => 'text/javascript')) !!}
-{!! \Html::script('assets/admin/pages/app/cliente/productos/nuevo-producto.js', array('type' => 'text/javascript')) !!}
+	{!! \Html::script('assets/global/plugins/bootstrap-select/bootstrap-select.min.js', array('type' => 'text/javascript')) !!}
+	{!! \Html::script('assets/global/plugins/select2/select2.min.js', array('type' => 'text/javascript')) !!}
+	{!! \Html::script('assets/global/plugins/jquery-multi-select/js/jquery.multi-select.js', array('type' => 'text/javascript')) !!}
+	{!! \Html::script('assets/admin/pages/app/cliente/productos/nuevo-producto.js', array('type' => 'text/javascript')) !!}
 @stop
 
 {{-- Inicializo los js --}}
 @section('init-js')
-	  NuevoProducto.init();
+	NuevoProducto.init();
 @stop
