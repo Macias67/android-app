db5b6e981be1e05f98b5f651ac2d18c365608127
Luis Macias <luismacias.angulo@gmail.com>
2015-12-20 22:04:24 -0600

    Galeria cambiando los mensajes a español

--- app/Library/UploadHanlder.php
+++ app/Library/UploadHanlder.php
@@ -22,24 +22,24 @@ class UploadHanlder
 	// PHP File Upload error message codes:
 	// http://php.net/manual/en/features.file-upload.errors.php
 	protected $error_messages = [
-		1                     => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-		2                     => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-		3                     => 'The uploaded file was only partially uploaded',
-		4                     => 'No file was uploaded',
-		6                     => 'Missing a temporary folder',
-		7                     => 'Failed to write file to disk',
-		8                     => 'A PHP extension stopped the file upload',
-		'post_max_size'       => 'The uploaded file exceeds the post_max_size directive in php.ini',
-		'max_file_size'       => 'File is too big',
-		'min_file_size'       => 'File is too small',
-		'accept_file_types'   => 'Filetype not allowed',
-		'max_number_of_files' => 'Maximum number of files exceeded',
-		'max_width'           => 'Image exceeds maximum width',
-		'min_width'           => 'Image requires a minimum width',
-		'max_height'          => 'Image exceeds maximum height',
-		'min_height'          => 'Image requires a minimum height',
-		'abort'               => 'File upload aborted',
-		'image_resize'        => 'Failed to resize image'
+		1                     => 'El archivo subido excede la directiva upload_max_filesize en php.ini',
+		2                     => 'El archivo subido excede la directiva MAX_FILE_SIZE que se especificó en el formulario HTML',
+		3                     => 'El archivo subido se ha subido sólo parcialmente',
+		4                     => 'No archivo fue subido',
+		6                     => 'Falta una carpeta temporal',
+		7                     => 'Error al escribir el archivo en el disco',
+		8                     => 'Una extensión de PHP detuvo la carga de archivos',
+		'post_max_size'       => 'El archivo subido excede la directiva post_max_size en php.ini',
+		'max_file_size'       => 'El archivo es demasiado grande',
+		'min_file_size'       => 'El archivo es demasiado pequeño',
+		'accept_file_types'   => 'Tipo de archivo no permitido',
+		'max_number_of_files' => 'Número máximo de archivos superó',
+		'max_width'           => 'Imagen excede ancho máximo',
+		'min_width'           => 'Imagen requiere un ancho mínimo',
+		'max_height'          => 'Imagen excede la altura máxima',
+		'min_height'          => 'Imagen requiere una altura mínima',
+		'abort'               => 'Carga de archivos abortado',
+		'image_resize'        => 'Error al cambiar el tamaño de la imagen'
 	];
 
 	protected $image_objects = [];
