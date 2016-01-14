220868616cb2bd57af80d3ce20cb6c06a3367c0f
Luis Macias <luismacias.angulo@gmail.com>
2016-01-13 23:41:01 -0600

    Probando con Amazon S3

--- config/path.php
+++ config/path.php
@@ -4,5 +4,5 @@ return [
 	'clientes' => 'src',
 	'temporal' => 'tmp',
 	//'storage'  => ($app->environment('local')) ? 'http://android.app/' : 'http://test-storage-app.storage.googleapis.com/'
-        'storage'  => ($app->environment('local')) ? 'http://android.app/' : 'https://s3-us-west-1.amazonaws.com/my-androidapp/'
+	'storage'  => ($app->environment('local')) ? 'http://android.app/' : 'https://s3.amazonaws.com/my-androidapp/'
 ];
